db679bc4b7c684f8b599702c045754ee78588fd1
Summer Shields <summershields@Summers-iMac.lan>
2025-04-19 15:57:39 -0600

    Getters and setters written. Engine class partially written and moved outside of car class.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,17 +2,67 @@ async function main() {
   //Car class, parameters (make, model, and year), properties (make, model, year, odometer, and engine)
   class Car {
     constructor(make, model, year) {
-      this.make = make;
-      this.model = model;
-      this.year = year;
+      this.make = make || "Generic";
+      this.model = model || "Generic";
+      this.year = year || "Unknown";
       this.odometer = 0;
       this.engine = false;
     }
 
-  //class Engine
-    //Properties (cylinderCount and isRunning)
-
     //getters and setters
+    get make() {
+      return this._make;
+    }
+    
+    get model() {
+      return this._model;
+    }
+
+    get year() {
+      return this._year;
+    }
+
+    get odometer() {
+      return this._odometer;
+    }
+    
+    set make(value) {
+      if (!value || typeof value !== "string") {
+        output("Invalid make provided. Defaulting to 'Generic'.");
+        this._make = "Generic";
+      }
+      else {
+        this._make = value;
+      }
+    }
+
+    set model(value) {
+      if (!value || typeof value !== "string") {
+        output("Invalid model provided. Defaulting to 'Generic'.");
+        this._model = "Generic";
+      }
+      else {
+        this._model = value;
+      }
+    }
+
+    set year(value) {
+      if (!value || typeof value !== "number" || value < 1900 || value > 2026) {
+        output("Invalid year provided. Defaulting to 'Unknown'.");
+      }
+      else {
+        this._year = value;
+      }
+    }
+
+    set odometer(value) {
+      if (!value || typeof value !== "number") {
+        output("Invalid odometer value provided.");
+      }
+      else {
+        this._odometer = value;
+      }
+    }
 
     //method 1 
     startEngine() {
@@ -30,7 +80,16 @@ async function main() {
     }
   }
 
+  class Engine {
+    constructor() {
+      this.cylinderCount = 0;
+      this.isRunning = false;
+    }
+  }
+  
+
   //Instantiate
+  let myEngine = new Engine()
 
   //JSON
 }
