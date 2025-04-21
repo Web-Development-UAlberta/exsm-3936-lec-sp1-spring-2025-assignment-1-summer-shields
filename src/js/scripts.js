b55d67077185fa10f1f4d963ebf06ea2f88d6cd2
Summer Shields <summershields@Summers-iMac.lan>
2025-04-20 20:19:39 -0600

    Bonus mark additions. Additions made to Car class including properties and methods, more additions made through transmission class and instantiations.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -40,15 +40,41 @@ async function main() {
       this.isRunning = false;
     }
   }
+
+  //Transmission class
+  class Transmission {
+    constructor(manualAutomatic, gearCount) {
+      this.manualAutomatic = manualAutomatic || "Automatic";
+      this.gearCount = gearCount || "Park";
+    }
+
+    setGear(newGear) {
+      newGear = String(newGear);
+      if (this.manualAutomatic == "Automatic") {
+        const validGears = ["Park", "Neutral", "Reverse", "Drive"];
+        if (!validGears.includes(newGear)) {
+          output("Invalid gear entered.");
+        }
+      } 
+      else if (this.manualAutomatic == "Manual") {
+        const validGears = ["Park", "Neutral", "Reverse", "1", "2", "3", "4", "5", "6"];
+        if (!validGears.includes(newGear)) {
+          output("Invalid gear entered.");
+        }
+      }
+      this.gearCount = newGear;
+    } 
+  }
   
   //Car class, parameters (make, model, and year), properties (make, model, year, odometer, and engine)
   class Car {
-    constructor(make, model, year, odometer, engine) {
+    constructor(make, model, year, odometer, engine, transmission) {
       this.make = make || "Generic";
       this.model = model || "Generic";
       this.year = year || "Unknown";
       this.odometer = odometer;
       this.engine = engine;
+      this.transmission = transmission;
     }
 
     //Getters and setters
@@ -108,14 +134,32 @@ async function main() {
 
     //Method 1 
     startEngine() {
-      this.engine.start();
-      output("Engine turned on.");
+      const gear = this.transmission.gearCount;
+      const isManual = this.transmission.manualAutomatic == "Manual";
+      if (this.engine.isRunning) {
+        output("The car is already running.");
+      }
+      else if (isManual && !(gear == "Neutral")) {
+        output("Cannot start. Gear needs to be in Neutral");
+      }
+      else if (!isManual && !["Neutral", "Park"].includes(gear)) {
+        output("Cannot start. Gear needs to be in Neutral or Park.");
+      }
+      else {
+        this.engine.start();
+        output("Engine turned on.");
+      }
     }
 
     //Method 2
     stopEngine() {
+      if (!this.engine.isRunning) {
+        output("The car is already tured off.");
+      }
+      else {
       this.engine.stop();
       output("Engine turned off.")
+      }
     }
 
     //Method 3
@@ -126,22 +170,47 @@ async function main() {
       else if (!this.engine.isRunning) {
         output("Engine is not running. Please turn on the engine to drive.");
       }
-      else {
-        this.odometer = this.odometer + int;
-        output(`Drove ${int} km.`)
+      else { //Transmissions added for the bonus marks section of the assignment
+        const gear = this.transmission.gearCount;
+        const isManual = this.transmission.manualAutomatic == "Manual";
+
+        if (isManual && !["1", "2", "3", "4", "5", "6"].includes(gear)) {
+          output("Cannot drive. Invalid gear while in Manual mode.");
+        }
+        else if (!isManual && gear !== "Drive") {
+          output("Cannot drive. Invalid gear while in Automatic mode.");
+        }
+        else {
+          this.odometer = this.odometer + int;
+          output(`Drove ${int} km.`);
+        }
       }
     }
+
+    //Shift method
+    shift(gear) {
+      this.transmission.setGear(gear);
+      output(`Gear set to ${gear}`);
+    }
   }
+  
 
   //Instantiate
   let myEngine = new Engine(4);
+
+  let myTransmission = new Transmission("Automatic");
   
-  let myCar = new Car("Ford", "Bronco", 2023, 0, myEngine);
+  let myCar = new Car("Ford", "Bronco", 2023, 0, myEngine, myTransmission);
+  myCar.shift("Park");
   myCar.startEngine();
+  myCar.shift("Drive");
   myCar.drive(100);
+  myCar.shift("Park");
   myCar.stopEngine();
   myCar.startEngine();
+  myCar.shift("Drive");
   myCar.drive(50);
+  myCar.shift("Park");
   myCar.stopEngine();
   output(`Total distance driven: ${myCar.odometer}km`);
 
@@ -149,3 +218,4 @@ async function main() {
   JSONcar = JSON.stringify(myCar);
   output(JSONcar);
 }
+
